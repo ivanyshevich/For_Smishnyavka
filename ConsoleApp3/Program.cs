c95b3ca2790d4c8bda7f89d9f145ec1d73d81eaa
ivanyshevich <KryvosheyaIvan@gmail.com>
2023-05-24 09:09:51 +0300

    Add files via upload

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Complex
+{
+    public double Real { get; set; }
+    public double Imaginary { get; set; }
+
+    public Complex(double real, double imaginary)
+    {
+        Real = real;
+        Imaginary = imaginary;
+    }
+
+    // Додавання двох комплексних чисел
+    public static Complex operator +(Complex c1, Complex c2)
+    {
+        return new Complex(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+    }
+
+    // Віднімання двох комплексних чисел
+    public static Complex operator -(Complex c1, Complex c2)
+    {
+        return new Complex(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
+    }
+
+    // Множення двох комплексних чисел
+    public static Complex operator *(Complex c1, Complex c2)
+    {
+        return new Complex(
+            c1.Real * c2.Real - c1.Imaginary * c2.Imaginary,
+            c1.Real * c2.Imaginary + c1.Imaginary * c2.Real);
+    }
+
+    // Перевантаження методу ToString для виведення комплексного числа
+    public override string ToString()
+    {
+        return $"{Real} + {Imaginary}i";
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Complex c1 = new Complex(1, 2);
+        Complex c2 = new Complex(3, 4);
+
+        Console.WriteLine("c1 = " + c1);
+        Console.WriteLine("c2 = " + c2);
+
+        Complex sum = c1 + c2;
+        Console.WriteLine("c1 + c2 = " + sum);
+
+        Complex difference = c1 - c2;
+        Console.WriteLine("c1 - c2 = " + difference);
+
+        Complex product = c1 * c2;
+        Console.WriteLine("c1 * c2 = " + product);
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public abstract class Figure
+{
+    public abstract double CalculateArea();
+}
+
+public class Circle : Figure
+{
+    public double Radius { get; set; }
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+}
+
+public class Square : Figure
+{
+    public double SideLength { get; set; }
+
+    public Square(double sideLength)
+    {
+        SideLength = sideLength;
+    }
+
+    public override double CalculateArea()
+    {
+        return Math.Pow(SideLength, 2);
+    }
+}
+
+public class Rectangle : Figure
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+
+    public Rectangle(double width, double height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public override double CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var figures = new Figure[]
+        {
+            new Circle(5.0),
+            new Square(4.0),
+            new Rectangle(3.0, 6.0)
+        };
+
+        foreach (var figure in figures)
+        {
+            Console.WriteLine($"Area of the {figure.GetType().Name} is: {figure.CalculateArea()}");
+        }
+    }
+}
